b41bad3ddcc792b66e1c910e6520999c35fb83bc
PaxzStealer <132356243+PaxzStealer@users.noreply.github.com>
2023-05-14 13:47:44 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -3,9 +3,5 @@ ToggleL = _G.OnOff -- if you want stop just type false
  while getgenv().ToggleL do wait()
             for i,v in pairs(game:GetService("Workspace")["__THINGS"].Orbs:GetChildren()) do
             v.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
-            for i,v in pairs(game:GetService("Workspace")["__THINGS"].Lootbags:GetChildren()) do
-            v.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
             end
         end
- end
-end
