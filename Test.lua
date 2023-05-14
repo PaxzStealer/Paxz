a89c5130b0d36ce909d5508c739d26a9fb238bf1
PaxzStealer <132356243+PaxzStealer@users.noreply.github.com>
2023-05-14 13:20:18 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,6 +1,6 @@
 local lib = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
 lib.Signal.Fire("Notification", _G.NotificationText, {
     color = Color3.fromRGB(103, 255, 80),
-    time = "_G.TextTime"
+    time = _G.TextTime
 })
 lib.Network.Fire("Sent Progress Notification")
