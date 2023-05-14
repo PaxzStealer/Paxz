b470e2064803ecead2572116d2ace1e96c689a87
PaxzStealer <132356243+PaxzStealer@users.noreply.github.com>
2023-05-14 13:29:58 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,1 +1,7 @@
-
+-- auto pickup orbs
+getgenv().ToggleL = _G.OnOff -- if you want stop just type false
+ while getgenv().ToggleL do wait()
+            for i,v in pairs(game:GetService("Workspace")["__THINGS"].Orbs:GetChildren()) do
+            v.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
+            end
+        end
